a3358f780d188b90ac910c3517dda976e0c19e3d
Dimas Muhammad <dimasjay2@gmail.com>
2026-04-27 16:14:36 +0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,7 +14,7 @@ export const Login = ({ onLoginSuccess }) => {
 
     try {
       // Menggunakan API Backend Asli (Tanpa Mock Data)
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch(import.meta.env.VITE_API_URL + '/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
